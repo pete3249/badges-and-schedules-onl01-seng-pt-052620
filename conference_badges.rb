5e7cdde933199599aa80e8a466e7c2963962f3bd
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-02 13:05:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  new_array = []
   array.each do |name|
-    puts "Hello, my name is #{name}."
+    new_array >> "Hello, my name is #{name}."
   end
-    
+ new_array
 end
